dd130bef09ebeef258586db9eb0ac17cf8fb5ae0
Manuel Taglieri <s257476@studenti.polito.it>
2021-05-05 16:41:28 +0200

    Versione risolta

--- src/main/java/it/polito/tdp/extflightdelays/model/Model.java
+++ src/main/java/it/polito/tdp/extflightdelays/model/Model.java
@@ -1,5 +1,124 @@
 package it.polito.tdp.extflightdelays.model;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.jgrapht.Graphs;
+import org.jgrapht.event.ConnectedComponentTraversalEvent;
+import org.jgrapht.event.EdgeTraversalEvent;
+import org.jgrapht.event.TraversalListener;
+import org.jgrapht.event.VertexTraversalEvent;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleWeightedGraph;
+import org.jgrapht.traverse.BreadthFirstIterator;
+
+import it.polito.tdp.extflightdelays.db.ExtFlightDelaysDAO;
+
 public class Model {
+	
+	private SimpleWeightedGraph<Airport, DefaultWeightedEdge> grafo;
+	private ExtFlightDelaysDAO dao;
+	private Map<Integer, Airport> idMap;
+	private HashMap<Airport, Airport> visita;
+	
+	public Model() {
+		dao = new ExtFlightDelaysDAO();
+		idMap = new HashMap<Integer, Airport>();
+		dao.loadAllAirports(idMap);
+	}
+	
+	public void creaGrafo(int x) {
+		grafo = new SimpleWeightedGraph<>(DefaultWeightedEdge.class);
+		
+		//aggiungo i vertici "filtrati"
+		Graphs.addAllVertices(grafo, dao.getVertici(x, idMap));
+		
+		//aggiungo gli archi
+		for (Rotta r : dao.getRotte(idMap)) {
+			if (this.grafo.containsVertex(r.getA1()) && this.grafo.containsVertex(r.getA2())) {
+				DefaultWeightedEdge e = this.grafo.getEdge(r.getA1(), r.getA2());
+				if (e == null) {
+					Graphs.addEdgeWithVertices(grafo, r.getA1(), r.getA2(), r.getN());
+				} else {
+					double pesoVecchio = this.grafo.getEdgeWeight(e);
+					double pesoNuovo = pesoVecchio + r.getN();
+					this.grafo.setEdgeWeight(e, pesoNuovo);
+				}
+			}
+		}
+		
+		System.out.println("Grafo creato");
+		System.out.println("# Vertici: "+grafo.vertexSet().size());
+		System.out.println("# Archi: "+grafo.edgeSet().size());
+		
+	}
+
+	public Set<Airport> getVertici() {
+		return this.grafo.vertexSet();
+	}
+	
+	public List<Airport> trovaPercorso(Airport a1, Airport a2) {
+		
+		List<Airport> percorso = new LinkedList<>();
+		
+		BreadthFirstIterator<Airport, DefaultWeightedEdge> it = new BreadthFirstIterator<>(grafo, a1);
+		
+		this.visita = new HashMap<>();
+		visita.put(a1, null);
+		
+		it.addTraversalListener(new TraversalListener<Airport, DefaultWeightedEdge>(){
+
+			@Override
+			public void connectedComponentFinished(ConnectedComponentTraversalEvent e) {
+			}
+
+			@Override
+			public void connectedComponentStarted(ConnectedComponentTraversalEvent e) {
+			}
+
+			@Override
+			public void edgeTraversed(EdgeTraversalEvent<DefaultWeightedEdge> e) {
+				Airport airport1 = grafo.getEdgeSource(e.getEdge());
+				Airport airport2 = grafo.getEdgeTarget(e.getEdge());
+				
+				if (visita.containsKey(airport1) && !visita.containsKey(airport2)) {
+					visita.put(airport2, airport1);
+				} else if (visita.containsKey(airport2) && !visita.containsKey(airport1)) {
+					visita.put(airport1, airport2);
+				}
+			}
+
+			@Override
+			public void vertexTraversed(VertexTraversalEvent<Airport> e) {
+			}
+
+			@Override
+			public void vertexFinished(VertexTraversalEvent<Airport> e) {
+			}
+		});
+		
+		//La visita del grafico
+		while(it.hasNext()) {
+			it.next();
+		}
+		
+		if (!visita.containsKey(a1) || !visita.containsKey(a2)) {
+			return null;
+		}
+		
+		percorso.add(a2);
+		Airport step = a2;
+		
+		while(visita.get(step)!=null) {
+			step = visita.get(step);
+			percorso.add(step);
+		}
+		
+		return percorso;
+		
+	}
 
 }
